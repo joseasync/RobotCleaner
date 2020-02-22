0e305df95284aa0e5fe625a69fbd0b7e905988ab
josecruzio <josericardo.developer@gmail.com>
2020-02-22 18:37:25 +0000

    Add project files.

---
+++ RobotCleaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RobotCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
