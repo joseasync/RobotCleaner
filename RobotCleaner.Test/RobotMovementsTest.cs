224b5ff049e54208040484131332b92ea97027b0
josecruzio <josericardo.developer@gmail.com>
2020-02-23 00:49:45 +0000

    RobotMovementsTest - Refactor/Supplementary tests

--- RobotCleaner.Test/RobotMovementsTest.cs
+++ RobotCleaner.Test/RobotMovementsTest.cs
@@ -41,5 +41,106 @@ namespace RobotCleaner.Test
 
         }
 
+        [Fact]
+        public void ShouldMoveToSouthLimit()
+        {
+            var movements = new List<Tuple<string, int>>()
+            {
+                Tuple.Create("E",2),
+                Tuple.Create("S",100050)
+            };
+            _myRobotCleaner = new RobotIA(movements.Count, 24, 41, movements);
+            _myRobotCleaner.Start();
+
+            Assert.True(_myRobotCleaner.GetOrientation() == "S");
+            Assert.True(_myRobotCleaner.GetPositionX() == 26);
+            Assert.True(_myRobotCleaner.GetPositionY() == NEGATIVE_LIMIT);
+        }
+
+        [Fact]
+        public void ShouldMoveToNorthLimit()
+        {
+            var movements = new List<Tuple<string, int>>()
+            {
+                Tuple.Create("W",2),
+                Tuple.Create("N",100050)
+            };
+            _myRobotCleaner = new RobotIA(movements.Count, 20, 41, movements);
+            _myRobotCleaner.Start();
+
+            Assert.True(_myRobotCleaner.GetOrientation() == "N");
+            Assert.True(_myRobotCleaner.GetPositionX() == 18);
+            Assert.True(_myRobotCleaner.GetPositionY() == POSITIVE_LIMIT);
+        }
+
+        [Fact]
+        public void ShouldMoveToWestLimit()
+        {
+            var movements = new List<Tuple<string, int>>()
+            {
+                Tuple.Create("S",2),
+                Tuple.Create("W",100050)
+            };
+            _myRobotCleaner = new RobotIA(movements.Count, 17, 21, movements);
+            _myRobotCleaner.Start();
+
+            Assert.True(_myRobotCleaner.GetOrientation() == "W");
+            Assert.True(_myRobotCleaner.GetPositionY() == 19);
+            Assert.True(_myRobotCleaner.GetPositionX() == NEGATIVE_LIMIT);
+        }
+
+        [Fact]
+        public void ShouldMoveToEastLimit()
+        {
+            var movements = new List<Tuple<string, int>>()
+            {
+                Tuple.Create("N",2),
+                Tuple.Create("E",100050)
+            };
+            _myRobotCleaner = new RobotIA(movements.Count, 20, 41, movements);
+            _myRobotCleaner.Start();
+
+            Assert.True(_myRobotCleaner.GetOrientation() == "E");
+            Assert.True(_myRobotCleaner.GetPositionY() == 43);
+            Assert.True(_myRobotCleaner.GetPositionX() == POSITIVE_LIMIT);
+        }
+
+        [Fact]
+        public void ShouldKeepFollowingCommandsAfterReachedOneLimit()
+        {
+            var movements = new List<Tuple<string, int>>()
+            {
+                Tuple.Create("N",2),
+                Tuple.Create("E",100050),
+                Tuple.Create("S",100),
+                Tuple.Create("W",50)
+            };
+            _myRobotCleaner = new RobotIA(movements.Count, 10, 10, movements);
+            _myRobotCleaner.Start();
+
+            Assert.True(_myRobotCleaner.GetOrientation() == "W");
+            Assert.True(_myRobotCleaner.GetPositionY() == -88);
+            Assert.True(_myRobotCleaner.GetPositionX() == 99950);
+        }
+
+        [Theory]
+        [InlineData("A")]
+        [InlineData("B")]
+        [InlineData("C")]
+        [InlineData("1")]
+        [InlineData("d")]
+        public void ShouldThrowErrorByInvalidOrientation(string orientation)
+        {
+            var movements = new List<Tuple<string, int>>()
+            {
+                Tuple.Create("E",2),
+                Tuple.Create(orientation,100010)
+            };
+            _myRobotCleaner = new RobotIA(movements.Count, 24, 1, movements);
+
+            Assert.Throws<NotSupportedException>(() => _myRobotCleaner.Start());
+
+        }
+
     }
 }
